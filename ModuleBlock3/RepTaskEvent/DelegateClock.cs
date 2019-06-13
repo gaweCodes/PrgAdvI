ab3e1f0a99e5cc8209f06cbfeb961213f2768e0f
Gabriel Weibel <gabriel.weibel@hotmail.de>
2019-06-13 19:19:32 +0200

    RepTask

---
+++ ModuleBlock3/RepTaskEvent/DelegateClock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace RepTaskEvent
+{
+    public class DelegateClock
+    {
+        public event EventHandler OnTick;
+
+        public void StartTimer()
+        {
+            const int seconds = 5;
+            var i = 0;
+            while (i < seconds)
+            {
+                Thread.Sleep(1000);
+                OnTick?.Invoke(this, new EventArgs());
+                i++;
+            }
+        }
+    }
+}
---
+++ ModuleBlock3/RepTaskEvent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RepTaskEvent
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var clock = new DelegateClock();
+            clock.OnTick += SayTick;
+            clock.OnTick += SayTack;
+            clock.StartTimer();
+        }
+
+        private static void SayTick(object sender, EventArgs e) => Console.WriteLine("Tick");
+        private static void SayTack(object sender, EventArgs e) => Console.WriteLine("Tack");
+    }
+}
+;
