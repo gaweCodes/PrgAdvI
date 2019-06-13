b248fe2fa8a0735ebc9c4528b70c0e647ff8e96c
Gabriel Weibel <gabriel.weibel@hotmail.de>
2019-06-13 18:56:14 +0200

    RetTask

---
+++ ModuleBlock3/RepTaskDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RepTaskDelegate
+{
+    internal static class Program
+    {
+        private delegate int Calculator(int number1, int number2);
+        private static void Main()
+        {
+            Calculator add = null;
+            add = Addition;
+            add += delegate(int a, int b) { return a + b; };
+            if (add != null)
+                Console.WriteLine(add(1,5));
+            Console.WriteLine(add?.Invoke(1, 5));
+            Console.ReadLine();
+        }
+
+        private static int Addition(int a, int b) => a + b;
+    }
+}
